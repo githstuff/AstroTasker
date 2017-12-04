87f35b0acff33221d6ee30b2828ca49dcc5f4adb
Aidan Colbert <colbert.box@gmail.com>
2017-12-03 22:28:33 -0500

    added glisten library to fix issue with ToggleButtonGroup

--- src/com/box/colbert/Controller.java
+++ src/com/box/colbert/Controller.java
@@ -1,4 +1,5 @@
 package com.box.colbert;
 
 public class Controller {
+    //TODO: all options, on click should display an Incomplete message when activated by mouse until functionality is implemented
 }
