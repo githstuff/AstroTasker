ce27dadd61726c23cbf463702db5b18f99a79fe4
Aidan Colbert <colbert.box@gmail.com>
2017-12-07 02:13:41 -0500

    gave ReccuringTask a CalendarWeek object which holds a LinkedHashmap that refers to whether a particular day of the week is toggled on

--- src/com/box/colbert/ClassTester.java
+++ src/com/box/colbert/ClassTester.java
@@ -1,5 +1,6 @@
 package com.box.colbert;
 
+
 import com.box.colbert.model.RecurringTask;
 import com.box.colbert.model.Task;
 
@@ -10,7 +11,12 @@ public class ClassTester {
     public static void main(String[] args){
         Task myTask = new Task("Do Something",1);
         System.out.println(myTask.toString());
-        RecurringTask myRecuring = new RecurringTask("Make Bed",2);
-        System.out.println(myRecuring);
+        RecurringTask myRecurring = new RecurringTask("Make Bed",2);
+        System.out.println(myRecurring);
+        System.out.println("Sunday was: "+myRecurring.week.isDayTrue("Sunday"));
+        myRecurring.week.toggleDay("Sunday");
+        System.out.println("Sunday is now "+ myRecurring.week.isDayTrue("Sunday"));
+        System.out.println("Should cause error: "+myRecurring.week.isDayTrue("day"));
+
     }
 }
--- src/com/box/colbert/model/RecurringTask.java
+++ src/com/box/colbert/model/RecurringTask.java
@@ -1,29 +1,19 @@
 package com.box.colbert.model;
 
-import com.yourkit.util.Strings;
-
-import java.util.ArrayList;
-
 /**
  * Created by Aidan on 12/5/2017.
  */
 public class RecurringTask extends Task {
     private int streak;
-    private CalendarWeek days;
+    public CalendarWeek week= new CalendarWeek();
     public RecurringTask(String description,int complexity){
         super(description, complexity);
-        CalendarWeek days= new CalendarWeek();
     }
 
     @Override
     public String toString() {
-        return super.toString();
+
+        return super.toString()+"[streak: "+streak+"] [days: "+week.toString();
     }
 
-    private class CalendarWeek{
-        private boolean sunday,monday,tuesday,wednesday,thursday,friday,saturday;
-        private CalendarWeek(){
-            sunday=monday=tuesday=wednesday=thursday=friday=saturday=true;
-        }
-    }
 }
